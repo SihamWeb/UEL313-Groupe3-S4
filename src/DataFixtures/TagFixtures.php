71d3d900941f0c56ac9e482d612d5a835d984618
Dylan LOUICHON <147518224+DylanLOUICHON@users.noreply.github.com>
2023-12-24 15:11:48 +0100

    Add tags for links

--- src/DataFixtures/TagFixtures.php
+++ src/DataFixtures/TagFixtures.php
@@ -11,30 +11,62 @@ class TagFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        $tag = new Tag();
-        $tag -> setTagName("internet");
+        $tagsData = [
+            "Moteur de recherche" => [
+                'https://google.com',
+                'https://duckduckgo.com/',
+                'https://www.bing.com/',
+                'https://www.deepl.com/translator'
+            ],
+            "Media" => [
+                'https://letelegramme.fr',
+                'https://www.lemonde.fr/',
+                'https://www.reddit.com/',
+                'https://discord.com/'
+            ],
+            "CVTIC" => [
+                'https://cvtic.unilim.fr/',
+            ],
+            "Références" => [
+                'https://developer.mozilla.org/fr/docs/Learn',
+                'https://www.wampserver.com/',
+                'https://www.cnil.fr/fr',
+                'https://fr.wikipedia.org'
+            ],
+            "Hébergement" => [
+                'https://www.ionos.fr',
+                'https://wordpress.com/fr/',
+                'https://www.php.net/manual/fr/index.php'
+            ],
+            "Actualités" => [
+                'https://www.lemonde.fr/',
+                'https://www.reddit.com/',
+                'https://discord.com/',
+                'https://www.onisep.fr'
+            ],
+            "Shopping en ligne" => [
+                'https://www.rueducommerce.fr',
+                'https://www.ldlc.com'
+            ],
+            "Outils de développement" => [
+                'https://www.https://validator.w3.org/.net/manual/fr/index.php',
+                'https://www.postman.com/',
+                'https://fr.wikipedia.org'
+            ]
+        ];
 
-        $manager -> persist($tag);
-        $liens = $manager->getRepository(Lien::class)->findBy(['lien_url' => ['https://google.com', 'https://letelegramme.fr']]);
+        foreach ($tagsData as $tagName => $links) {
+            $tag = new Tag();
+            $tag->setTagName($tagName);
+            $manager->persist($tag);
 
-        foreach ($liens as $lien) {
-            $tag->addLien($lien);
+            $liens = $manager->getRepository(Lien::class)->findBy(['lien_url' => $links]);
+
+            foreach ($liens as $lien) {
+                $tag->addLien($lien);
+            }
+
+            $manager->flush();
         }
-        $manager->flush();
-
-
-
-        $tag = new Tag();
-        $tag -> setTagName("journal");
-
-        $manager -> persist($tag);
-        $liens = $manager->getRepository(Lien::class)->findBy(['lien_url' => ['https://letelegramme.fr']]);
-
-        foreach ($liens as $lien) {
-            $tag->addLien($lien);
-        }
-        $manager->flush();
     }
 }
